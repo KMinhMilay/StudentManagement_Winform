bc664e2226a9a303b16dae699a33f45637c851d1
NguyenHoangTrung <trunghoangnguyen.it@gmail.com>
2024-04-30 18:23:18 +0700

    get personal data, change password, update personal data

--- QuanLiHocSinh/DAO/AuthDAO.cs
+++ QuanLiHocSinh/DAO/AuthDAO.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace QuanLiHocSinh.DAO
 {
@@ -29,6 +30,42 @@ namespace QuanLiHocSinh.DAO
                 return new Account(data.Rows[0]);
             }
             return null;
+        }
+        public StudentPersonalInformation GetStudentPersonalInfo(string id)
+        {
+            DataTable data;
+
+            data = DataProvider.Instance.ExecuteQuery("SP_GetStudentPersonalInfo @id", [id]);
+
+            if (data.Rows.Count > 0)
+            {
+                return new StudentPersonalInformation(data.Rows[0]);
+            }
+            return null;
+        }
+        public TeacherPersonalInformation GetTeacherPersonalInfo(string id)
+        {
+            DataTable data;
+
+            data = DataProvider.Instance.ExecuteQuery("SP_GetTeacherPersonalInfo @id", [id]);
+
+            if (data.Rows.Count > 0)
+            {
+                return new TeacherPersonalInformation(data.Rows[0]);
+            }
+            return null;
+        }
+        public bool UpdateStudentPersonalInfoSuccess(string id, string lastname, string firstname, DateTime birthdate, string gender, string hometown, string address, string email, string phoneNumber, string parentName, string parentPhoneNumber)
+        {
+            return DataProvider.Instance.ExecuteNonQuery("SP_UpdateStudentPersonalInfo @id , @lastname , @firstname , @birthdate , @gender , @hometown , @address , @email , @phoneNumber , @parentName , @parentPhoneNumber", [id, lastname, firstname, birthdate, gender, hometown, address, email, phoneNumber, parentName, parentPhoneNumber]) > 0;
+        }
+        public bool UpdateTeacherPersonalInfoSuccess(string id, string lastname, string firstname, DateTime birthdate, string gender, string hometown, string address, string email, string phoneNumber)
+        {
+            return DataProvider.Instance.ExecuteNonQuery("SP_UpdateTeacherPersonalInfo @id , @lastname , @firstname , @birthdate , @gender , @hometown , @address , @email , @phoneNumber", [id, lastname, firstname, birthdate, gender, hometown, address, email, phoneNumber]) > 0;
+        }
+        public bool changePasswordSuccess(string id, string password, string accountType)
+        {
+            return DataProvider.Instance.ExecuteNonQuery("SP_ChangePassword @id , @password , @accountType", [id, password, accountType]) > 0;
         }
     }
 }
